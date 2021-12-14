32c895181ca89f773206a4b18ab23e08ef0de5dc
Federico Zappone <46301736+federicozappone@users.noreply.github.com>
2021-12-14 15:33:15 +0100

    updated to CAHV camera model

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -3,6 +3,41 @@ import numpy as np
 import os
 import glob
 
+def compute_rotation_matrix(w, phi, k, degrees=False):
+    """
+    Computes Rotation matrix from the rotation angle w, phi, k
+    related to x, y and z.
+    Returns
+    -------
+    r_matrix: Rotational Matrix M
+         [cos(phi)cos(k)   sin(w)sin(phi)cos(k)+cos(w)sin(k)   -cos(w)sin(phi)cos(k)+sin(w)sin(k)]
+     M = [-cos(phi)sin(k)  -sin(w)sin(phi)sin(k)+cos(w)cos(k)  cos(w)sin(phi)sin(k)+sin(w)cos(k) ]
+         [   sin(phi)              -sin(w)cos(phi)                        cos(w)cos(phi)         ]
+    """
+
+    if degrees is True:
+        w = math.radians(w)
+        phi = math.radians(phi)
+        k = math.radians(k)
+
+    # Rotational Matrix M generation
+    r_matrix = np.zeros((3, 3))
+    r_matrix[0, 0] = math.cos(phi) * math.cos(k)
+    r_matrix[0, 1] = math.sin(w) * math.sin(phi) * math.cos(k) + \
+        math.cos(w) * math.sin(k)
+    r_matrix[0, 2] = - math.cos(w) * math.sin(phi) * math.cos(k) + \
+        math.sin(w) * math.sin(k)
+    r_matrix[1, 0] = - math.cos(phi) * math.sin(k)
+    r_matrix[1, 1] = - math.sin(w) * math.sin(phi) * math.sin(k) + \
+        math.cos(w) * math.cos(k)
+    r_matrix[1, 2] = math.cos(w) * math.sin(phi) * math.sin(k) + \
+        math.sin(w) * math.cos(k)
+    r_matrix[2, 0] = math.sin(phi)
+    r_matrix[2, 1] = - math.sin(w) * math.cos(phi)
+    r_matrix[2, 2] = math.cos(w) * math.cos(phi)
+
+    return r_matrix
+
 pattern_size = (6, 9) # chessboard size
 
 subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
@@ -62,4 +97,20 @@ print("image shape:", img_shape[::-1])
 print("K:", K.tolist())
 print("D:", D.tolist())
 
-np.savez("calibration_data/calibration.npz", K=K, D=D)
+center = np.zeros(3)
+rotation_mat = compute_rotation_matrix(0, 0, 0)
+
+fx = K[0, 0]
+fy = K[1, 1]
+cx = K[0, 2]
+cy = K[1, 2]
+
+C = center
+A = rotation_mat[2, :] - C
+Hn = rotation_mat[0, :] - C
+Vn = rotation_mat[1, :] - C
+
+H = fx * Hn + cx * A
+V = fy * Vn + cy * A
+
+np.savez("calibration_data/calibration.npz", C=C, A=A, H=H, V=V, D=D)
